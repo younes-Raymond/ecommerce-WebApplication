de57af1416d92569507f547a3fdad090782e44e2
younes1337 <raymondyounes2@gmail.com>
2022-12-21 20:46:47 +0100

    #

--- src/map.js
+++ src/map.js
@@ -1,3 +1,4 @@
+
 const mymap = L.map('issmap').setView([0, 0], 1);
 const attribution = 
 '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
@@ -16,15 +17,10 @@ const issicon = L.icon({
 const marker = L.marker([0,0],{icon:issicon}).addTo(mymap);
 const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
   
-  // set this function every 1seconde
+  // set this function every 1 seconde
   window.setInterval('getISS()',1000);
   //async the fuction getiss and make wait the fecth to fetch data 
   async function getISS () {
-
-
-   //    const response = await fetch(api_url);
-   //    const data = await response.json();
-   //    const {latitude,longitude} = data;
    
 if ('geolocation' in navigator) {
 console.log("geolocation is avilable")
@@ -42,3 +38,36 @@ alert("please give us the premession from setting up and try again ")
 
 getISS()
 
+
+function SendDataToPostMan () {
+
+const body = document.querySelector("body");
+const confirm_BTN = document.getElementById("confirm_BTN");
+const yes = document.getElementById("yes");
+const no = document.getElementById("no");
+
+yes.onclick = () => {
+  yes.classList.add("active")
+  no.classList.remove("active")
+}
+no.onclick = () => {
+  no.classList.add("active")
+  yes.classList.remove("active")
+
+}
+window.onload = () => {
+  yes.classList.add("active")
+}
+
+confirm_BTN.onclick = (e) => {
+  if(no.classList.contains("active")) {
+    no.style.setProperty("pointer:events","none");
+    
+
+  } else if (yes.classList.contains("active")) {
+    // send data to the server and save it in data base but github host deosn't support server side
+  }
+}
+}
+
+SendDataToPostMan()
