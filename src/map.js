c2b00aa9eac736d7c698b24935c5d66ae5dc9d76
younes1337 <raymondyounes2@gmail.com>
2022-12-24 21:50:18 +0100

    new feat

--- src/map.js
+++ src/map.js
@@ -1,45 +1,6 @@
 
-const mymap = L.map('issmap').setView([0, 0], 1);
-const attribution = 
-'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-const tiles = L.tileLayer(tileURL,{attribution});
-tiles.addTo(mymap);
-
-
-// making a marker with a custom icon 
-const issicon = L.icon({
-  iconUrl:'images/location.png',
-  iconSize: [50,32],
-  iconAnchor: [25,16]
-});
-
-const marker = L.marker([0,0],{icon:issicon}).addTo(mymap);
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
-  
-  // set this function every 1 seconde
-  window.setInterval('getISS()',1000);
-  //async the fuction getiss and make wait the fecth to fetch data 
-  async function getISS () {
-   
-if ('geolocation' in navigator) {
-console.log("geolocation is avilable")
-navigator.geolocation.getCurrentPosition(async position => {
-const latitude = position.coords.latitude;
-const longitude = position.coords.longitude;
-const data = {latitude,longitude};
-marker.setLatLng([latitude,longitude])
-      mymap.setView([latitude,longitude],15)
-});
-} else {
-alert("please give us the premession from setting up and try again ")
-}
-  }
-
-getISS()
-
-
-function SendDataToPostMan () {
+// import { sendlocation } from './master1';
+// console.log(sendlocation())
 
 const body = document.querySelector("body");
 const confirm_BTN = document.getElementById("confirm_BTN");
@@ -58,16 +19,91 @@ no.onclick = () => {
 window.onload = () => {
   yes.classList.add("active")
 }
-
 confirm_BTN.onclick = (e) => {
   if(no.classList.contains("active")) {
-    no.style.setProperty("pointer:events","none");
-    
-
+    alert("dont send datra ")
   } else if (yes.classList.contains("active")) {
-    // send data to the server and save it in data base but github host deosn't support server side
+  const agree = prompt("do you want to confirm order (s) with picture","yes");
+    if(agree === "yes") {
+      window.location.href = "index2.html";
   }
 }
 }
 
-SendDataToPostMan()
+
+function sendlocation () {
+  if ('geolocation' in navigator) {
+    console.log("geolocation is avilable")
+    navigator.geolocation.getCurrentPosition(async position => {
+ const lat = position.coords.latitude;
+ const lon = position.coords.longitude;
+const data = {lat,lon};
+const options = {
+    method: 'POST',
+    headers: {
+        'Content-Type':'application/json'
+    },
+    body: JSON.stringify(data)
+}
+ fetch("/api", options);
+ 
+//  .then(response => {
+  // console.log(response)
+// })
+
+//  const dataa = await response.json();
+//  console.log(dataa);
+ });
+} else {
+    console.log("erour");
+}
+}
+
+
+
+
+const mymap = L.map('issmap').setView([0, 0], 1);
+const attribution = 
+'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+const tileURL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileURL,{attribution});
+tiles.addTo(mymap);
+
+
+// making a marker with a custom icon 
+const issicon = L.icon({
+  iconUrl:'images/location.png',
+  iconSize: [50,32],
+  iconAnchor: [25,16]
+});
+
+const marker = L.marker([0,0],{icon:issicon}).addTo(mymap);
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544';
+  
+  // set this function every 1seconde
+  window.setInterval('getISS()',1000);
+  //async the fuction getiss and make wait the fecth to fetch data 
+  async function getISS () {
+
+
+   //    const response = await fetch(api_url);
+   //    const data = await response.json();
+   //    const {latitude,longitude} = data;
+   
+if ('geolocation' in navigator) {
+console.log("geolocation is avilable")
+navigator.geolocation.getCurrentPosition(async position => {
+const latitude = position.coords.latitude;
+const longitude = position.coords.longitude;
+const data = {latitude,longitude};
+marker.setLatLng([latitude,longitude])
+      mymap.setView([latitude,longitude],15)
+});
+} else {
+alert("please give us the premession from setting up and try again ")
+}
+  }
+getISS()
+
+
+
